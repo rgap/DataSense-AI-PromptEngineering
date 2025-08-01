49e0dc18ad18f9f043c9957eb10af725ee5b6add
rgap <r.guzmanap@gmail.com>
2025-08-01 17:36:46 -0500

    fix: reparar el error usual de vercel 404 al hacer refresh, etc agregando el vercel.json

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import AppLayout from "./layouts/AppLayout";
 import DashboardView from "./views/DashboardView";
 import HomeView from "./views/HomeView";
@@ -12,6 +12,8 @@ export default function Router() {
           <Route path="/" element={<HomeView />} />
           <Route path="/analizando" element={<LoadingView />} />
           <Route path="/resultados" element={<DashboardView />} />
+          {/* Redirect any unknown routes to home */}
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
     </BrowserRouter>
